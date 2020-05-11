3d4e8d10a27dce4ade39aafc9c3c73979c223324
Eduardo Bonilla Rodriguez <ebonilla@redhat.com>
2020-05-11 03:47:15 -0400

    config

---
+++ satellite/files/generate_certs.sh
@@ -0,0 +1,3 @@
+capsule-certs-generate \
+--foreman-proxy-fqdn node-2.edutest.lab.upshift.rdu2.redhat.com \
+--certs-tar /root/capsule_cert/capsule_certs.tar
---
+++ satellite/tasks/satellite-installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+satellite-installer --scenario satellite \
+--foreman-initial-organization "initial_organization_name" \
+--foreman-initial-location "initial_location_name" \
+--foreman-initial-admin-username admin_user_name \
+--foreman-initial-admin-password admin_password
