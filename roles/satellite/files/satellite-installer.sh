f334b4c0388a30a6b01cde873f4aa5259789b10c
quicklab <quicklab@node-0.eduaut.lab.pnq2.cee.redhat.com>
2020-05-19 10:01:40 -0400

    satellite

--- roles/satellite/files/satellite-installer.sh
+++ roles/satellite/files/satellite-installer.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-satellite-installer --scenario satellite \
---foreman-initial-organization "initial_organization_name" \
---foreman-initial-location "initial_location_name" \
---foreman-initial-admin-username admin_user_name \
---foreman-initial-admin-password admin_password
-
+sat
